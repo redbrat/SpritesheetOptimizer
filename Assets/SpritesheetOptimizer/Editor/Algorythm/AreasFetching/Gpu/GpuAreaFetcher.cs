a9768b8aae42e2b79458f8e0d3c48c14bc95314f
Andrey Tsarkov <tm-tsarkov@yandex.ru>
2019-08-12 22:28:18 +0300

    GPU algorythm scaffolding enhanced

---
+++ Assets/SpritesheetOptimizer/Editor/Algorythm/AreasFetching/Gpu/GpuAreaFetcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GpuAreaFetcher : IAreaFetcher
+{
+    private readonly ComputeShader _computeShader;
+
+    public GpuAreaFetcher()
+    {
+        _computeShader = Resources.Load<ComputeShader>("SpritesheetOptimizer/Shaders/AreasFetchingShader");
+    }
+
+    Task<ConcurrentDictionary<int, MyArea>> IAreaFetcher.FetchAreas(MyColor[][][] sprites, IEnumerable<MyVector2> areaSizings, IAreaEnumerator areaEnumerator, ProgressReport progressReport)
+    {
+        var algorythmKernel = _computeShader.FindKernel("CSMain");
+        var (groupSizeX, groupSizeY, groupSizeZ) = _computeShader.GetKernelThreadGroupSizes(algorythmKernel);
+
+        throw new System.NotImplementedException();
+    }
+
+    private ComputeBuffer getSpritesBuffer(MyColor[][][] sprites)
+    {
+        var bufferSize = sprites.Length * sprites[0].Length * sprites[0][0].Length;
+        var result = new ComputeBuffer(bufferSize, 32);
+        result.SetData(sprites);
+
+        return result;
+    }
+}
---
+++ Assets/SpritesheetOptimizer/Editor/Algorythm/AreasFetching/Gpu/GpuUtilities.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class GpuUtilities
+{
+    public static (uint x, uint y, uint z) GetKernelThreadGroupSizes(this ComputeShader shader, int kernelIndex)
+    {
+        shader.GetKernelThreadGroupSizes(kernelIndex, out uint x, out uint y, out uint z);
+        return (x, y, z);
+    }
+}
