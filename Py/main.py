cf106975c75419a80e2a31df59cd416ed9eb25f3
Andrey Tsarkov <tm-tsarkov@yandex.ru>
2019-11-17 17:34:13 +0300

    ok, parallel nopython numba function implemented with petty results

--- Py/main.py
+++ Py/main.py
@@ -78,18 +78,9 @@ def initializeCountsFromLists(sizingsArg, allListsArg):
 
 def initializeCountsFromArrays(sizings, allArrays):
     result = []
-    for s in range(len(sizings)):
-        sizingX = sizings[s][0]
-        sizingY = sizings[s][1]
-        # print("size: " + str(sizingX) + "," + str(sizingY))
-        result.append([])
-        shape = allArrays.shape
-        for l in range(shape[0]):
-            width = shape[1] - sizingX
-            height = shape[2] - sizingY
-            #print(str(len(allListsArg[l])) + "," + str(len(allListsArg[l][0])))
-            #print(str(width) + "," + str(height))
-            result[s].append(twoDList(0, width, height))
+    for a in range(len(allArrays)):
+        shape = allArrays[a].shape
+        result.append(np.full((sizings.shape[0], shape[0], shape[1]), 0))
     return result
 
 
@@ -99,7 +90,7 @@ initialPixelsUnprocessed = unprocessedPixels
 
 
 @jit(nopython=True)
-def countSample(sampleL, sampleX, sampleY, sampleWidth, sampleHeight, allLists):
+def countSampleList(sampleL, sampleX, sampleY, sampleWidth, sampleHeight, allLists):
     result = 0
     repeat = False
     for l in range(len(allLists)):
@@ -143,34 +134,87 @@ def countSample(sampleL, sampleX, sampleY, sampleWidth, sampleHeight, allLists):
     return result
 
 
-with progressbar.ProgressBar(max_value=len(sizingsList)) as bar:
+@jit(nopython=True, parallel=True)
+def countSampleOnArray(sampleNdArr, sampleA, sampleX, sampleY, sampleWidth, sampleHeight, candidateNdArray, candidateA, sizings):
+    result = 0
+    repeat = False
+
+    sizingsCount = sizings.shape[0]
+    shape = candidateNdArray.shape
+    width = shape[0]
+    height = shape[1]
+    xDomain = width - sampleWidth
+    yDomain = height - sampleHeight
+
+    for s in range(sizingsCount):
+        for x in range(xDomain):
+            for y in range(yDomain):
+                maybeThis = True
+                for xx in range(sampleWidth):
+                    for yy in range(sampleHeight):
+                        sampleXX = sampleX + xx
+                        sampleYY = sampleY + yy
+                        samplePixel = sampleNdArr[sampleXX][sampleYY]
+
+                        candidateXX = x + xx
+                        candidateYY = y + yy
+                        candidatePixel = candidateNdArray[candidateXX][candidateYY]
+
+                        if (samplePixel[3] == 0 and candidatePixel[3] == 0):
+                            continue
+
+                        if (samplePixel[0] != candidatePixel[0] or samplePixel[1] != candidatePixel[1] or samplePixel[2] != candidatePixel[2] or samplePixel[3] != candidatePixel[3]):
+                            maybeThis = False
+                            break
+
+                    if (maybeThis == False):
+                        break
+
+                if (maybeThis):
+                    if (candidateA < sampleA or (candidateA == sampleA and x < sampleX) or (candidateA == sampleA and x == sampleX and y < sampleY)):
+                        repeat = True
+                        result = 0
+                        break
+                    else:
+                        result = result + 1
+            if (repeat == True):
+                break
+        if (repeat == True):
+            break
+    return result
+
     # while unprocessedPixels > 0:
     # print("sampleCounts shape: " + str(len(sampleCounts)) + "," +
     #      str(len(sampleCounts[0])) + "," + str(len(sampleCounts[0][0])) + "," + str(len(sampleCounts[0][0][0])))
-    sampleCounts = initializeCountsFromLists(sizingsList, allLists)
-    for s in range(len(sizingsList)):
-        sizingX = sizingsList[s][0]
-        sizingY = sizingsList[s][1]
-        for l in range(len(allLists)):
-            sampleSprite = allLists[l]
-            width = len(sampleSprite)
-            height = len(sampleSprite[0])
+
+
+with progressbar.ProgressBar(max_value=len(allNdArrays)) as bar:
+    sampleCounts = initializeCountsFromArrays(sizings, allNdArrays)
+    arraysLen = len(allNdArrays)
+    rangeOfAllNdArrays = range(arraysLen)
+    for sampleA in rangeOfAllNdArrays:
+        sampleNdArr = allNdArrays[sampleA]
+        shape = sampleNdArr.shape
+        width = shape[0]
+        height = shape[1]
+        sizingsLen = sizings.shape[0]
+        for s in range(sizingsLen):
+            sizingX = sizings[s, 0]
+            sizingY = sizings[s, 1]
             sampleDomainX = width - sizingX
             sampleDomainY = height - sizingY
-
-            #print("width = " + str(width))
-            #print("height = " + str(height))
-            #print("sizingX = " + str(sizingX))
-            #print("sizingY = " + str(sizingY))
+            # print("shape = " + str(shape) + ". sizingX: " + str(sizingX) + ", sizingY: " + str(sizingY) + ", sampleDomainX: " +
+            #      str(sampleDomainX) + ", sampleDomainY: " + str(sampleDomainY))
             for sampleX in range(sampleDomainX):
                 for sampleY in range(sampleDomainY):
-                    #print("sampleX = " + str(sampleX))
-                    #print("sampleY = " + str(sampleY))
-                    #print("l = " + str(l))
-                    #print("s = " + str(s))
-                    sampleCounts[s][l][sampleX][sampleY] = countSample(
-                        l, sampleX, sampleY, sizingX, sizingY, allLists)
-        bar.update(s)
+                    counts = np.zeros(arraysLen)
+                    for candidateA in rangeOfAllNdArrays:
+                        counts[candidateA] = countSampleOnArray(
+                            sampleNdArr, sampleA, sampleX, sampleY, sizingX, sizingY, allNdArrays[candidateA], candidateA, sizings)
+                    # print(str(sampleA))
+                    # print(sampleCounts[sampleA].shape)
+                    sampleCounts[sampleA][s][sampleX][sampleY] = counts.sum()
+        bar.update(sampleA)
 
     #unprocessedPixels = countListsOpaquePixels(allLists)
     #bar.update(initialPixelsUnprocessed - unprocessedPixels)
